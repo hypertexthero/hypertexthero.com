f1469513a1234e92ffe84be9fabe29042f0f4548
hypertexthero <simon@hypertexthero.com>
2013-04-17 16:07:03 +0200

    slight improvement to smart_truncate functino

--- views.py
+++ views.py
@@ -57,7 +57,8 @@ def smart_truncate(content, length=200, suffix='...'):
     if len(content) <= length:
         return content
     else:
-        return ' '.join(content[:length+1].split(' ')[0:-1]) + suffix
+        # return ' '.join(content[:length+1].split(' ')[0:-1]) + suffix
+        return content[:length].rsplit(' ', 1)[0]+suffix
 
 class RssLogbookFeed(Feed):
     title = "Hypertexthero Logbook by Simon Griffee"
