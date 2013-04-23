10d76e74e9f52add1a19b65322321b36aca6f472
hypertexthero <simon@hypertexthero.com>
2013-04-23 15:14:47 +0200

    more work, medusa work renderer initial attempt

--- renderers.py
+++ renderers.py
@@ -21,7 +21,7 @@ from hth.models import Entry
 
 class EntryRenderer(StaticSiteRenderer):
     def get_paths(self):
-        paths = ["/", "/logbook/", ]
+        paths = ["/", "/logbook/", "/work/", ]
 
         items = Entry.objects.filter(is_active=True).order_by('-pub_date')
         for item in items:
@@ -30,3 +30,32 @@ class EntryRenderer(StaticSiteRenderer):
         return paths
 
 renderers = [EntryRenderer, ]
+
+# =todo
+# from hth.models import Entry
+# from django.contrib.flatpages.models import FlatPage
+# 
+# from itertools import chain
+# 
+# class EntryRenderer(StaticSiteRenderer):
+#     def get_paths(self):
+#         paths = ["/", "/logbook/", "/work/", ]
+#         entry_list = []
+#         work_list = []
+#         result_list = list(chain(entry_list, work_list))
+# 
+#         entry_list = Entry.objects.filter(is_active=True).order_by('-pub_date')
+#         for entry in entry_list:
+#             paths.append(entry.get_absolute_url())
+# 
+#         work_list = FlatPage.objects.all()
+#         for work in work_list:
+#             paths.append(work.get_absolute_url())            
+# 
+#         result_list = sorted(
+#             chain(entry_list, work_list),
+#             key=lambda instance: instance)
+# 
+#         return paths
+# 
+# renderers = [EntryRenderer, ]
