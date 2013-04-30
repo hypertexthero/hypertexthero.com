bb1910c76f34c3267a352417a5e4a419c7dd5268
Simon Griffee <simon@hypertexthero.com>
2013-04-30 12:18:53 +0000

    settings

--- settings.py
+++ settings.py
@@ -31,27 +31,6 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = False
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(DIRNAME, 'static/files')
-
-# ln -s ~/Projects/hth-env/lib/python2.7/site-packages/django/contrib/admin/static/admin/ ~/projects/hth-env/hth/hth/static/admin
-ADMIN_MEDIA_PREFIX = '/static/admin/'
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(DIRNAME, 'static')
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    "/static/",
-)
-
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
@@ -138,7 +117,6 @@ INSTALLED_APPS = (
     'contact.templatetags',
     'hth.templatetags',
     
-    'taggit',
     'hth',
 )
 
